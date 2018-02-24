60a34680a9a2cbcc27dffb641e9ab1fc642717ea
tommy1019 <tommy.stanley96@gmail.com>
2018-02-24 15:29:22 -0600

    Initial Commit

---
+++ TerraDrop/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TerraDrop
+//
+//  Created by Tommy Stanley on 2/24/18.
+//  Copyright © 2018 Tommy Stanley. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
